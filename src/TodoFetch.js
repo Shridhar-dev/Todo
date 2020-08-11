fc20f94981b3dae7b11ce21cd8875068272161aa
Shridhar-dev <shridharkamat10@gmail.com>
2020-08-11 20:45:05 +0530

    first one

--- src/TodoFetch.js
+++ src/TodoFetch.js
@@ -3,6 +3,7 @@ import db from './firebase'
 
 let indexer;
 let arr_indexer;
+
 class Todo extends React.Component{
     constructor(){
         super()
@@ -10,7 +11,8 @@ class Todo extends React.Component{
             name:[],
             formval:'',
             eformval:'',
-            loading:true
+            loading:true,
+            eformdisplay:'none'
             
         }
         this.onDelete = this.onDelete.bind(this)
@@ -18,19 +20,29 @@ class Todo extends React.Component{
         this.handleSubmit = this.handleSubmit.bind(this)
         
     }
+
+    /*Fetching Data from firebase*/
+
     componentWillMount(){
-        db.collection('Todoitems').get().then((snaper)=>{
-            snaper.docs.forEach((snap)=>{
+        db.collection('Todoitems').onSnapshot((snaper)=>{
+            let changes = snaper.docChanges();
+            changes.forEach((change)=>{
+                if(change.type === "added"){  
                 this.setState(prevState=>{
                     return{
-                        name:[...prevState.name,snap],
+                        name:[...prevState.name,change.doc],
                         loading:false
                     }
                     })
-                
+            }
             })
         })
     }
+
+    /*Methods*/
+
+    /* deleting items from firebase and array when we click on X button */
+
     onDelete(e){
         e.preventDefault();
         indexer = e.target.parentElement.getAttribute("dataId");
@@ -42,40 +54,50 @@ class Todo extends React.Component{
             name:narray.splice(arr_indexer,1) ,
         }) 
     }
+
+    /* Updating State variables relating to forms  */
+
     handleChange(e){
-        let form = e.target
-        if(form.name === "new-item"){
+        let name = e.target.name
+        let value = e.target.value
+       
             this.setState({
-                formval:form.value
+                [name]:value
             })
-            console.log(this.state.formval)
+            console.log([name])
 
         }
-        else if(form.name === "edit-item"){
-            this.setState({
-                eformval:form.value
-            })
-            console.log(this.state.eformval)
-        }
-    }
+    
+
+
+    /* Updating State variables relating to Buttons(delete,edit,show edit form) */
+
     handleSubmit(e){
         e.preventDefault();
         if(e.target.name === "addbutton"){
-        db.collection('Todoitems').add({Item:this.state.formval})
+            db.collection('Todoitems').add({Item:this.state.formval})
+                     
         }
         else if(e.target.name === "edb"){
             indexer = e.target.parentElement.getAttribute("dataId");
-        
-
+            this.setState({
+                eformdisplay:'inline'
+            })
         }
-        else if(e.target.name === "editbutton"){
-            
+        else if(e.target.name === "editbutton"){ 
             db.collection('Todoitems').doc(indexer).update({Item:this.state.eformval})
-
+            this.setState({
+                eformdisplay:'none'
+            })
         }
     }
     
+    /* Rendering of JSX */
+
     render(){ 
+
+        /* Mapping over array of objects */
+
         const items = this.state.name.map((d)=>{
             return(
             <li dataId={d.id }id={this.state.name.indexOf(d) }>
@@ -84,25 +106,32 @@ class Todo extends React.Component{
                 <button name='edb' onClick={this.handleSubmit} >Edit</button>
             </li>
             )
-        })        
+        }) 
+        
+        /*To prevent errors we check whether we have retrievied all the data from firebase */
+        
         if(this.state.loading === true){
             return(
             <h2>Wait</h2>
             )
         }
+
+        /* Main rendering area */
+
         else{
             return( 
             <>
             <h2>Todo</h2>
             <form>
-                <input name="new-item" placeholder="new item" onChange={this.handleChange}></input>
+                <input name="formval" placeholder="new item" onChange={this.handleChange}></input>
                 <button onClick={this.handleSubmit} name="addbutton">Submit</button>
             </form>
-            <form>
-                <input name="edit-item" placeholder="edit item" onChange={this.handleChange}></input>
-                <button onClick={this.handleSubmit} name="editbutton">Submit</button>
+            <form style={{display:this.state.eformdisplay}}>
+                <input name="eformval" placeholder="edit item" onChange={this.handleChange}></input>
+                <button onClick={this.handleSubmit} name="editbutton">Edit</button>
             </form>
             
+            {/* Show all items*/}
 
             <h2>{items}</h2>
             </>
